ebbf3f1b6f8cfd5a7b30d1d4a54c6d34aad20ffb
TiodorBurilkov <109085379+TiodorBurilkov@users.noreply.github.com>
2022-07-11 17:01:11 +0300

    Working solution

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Point pointA = new Point();
+        Point pointB = new Point();
+        Point pointC = new Point();
+        Point pointD = new Point();
+        double sinus = 0;
+
+        System.out.println("Enter the coordinates of A ");
+        pointA.setX(scanner.nextInt());
+        pointA.setY(scanner.nextInt());
+
+        System.out.println("Enter the coordinates of B ");
+        pointB.setX(scanner.nextInt());
+        pointB.setY(scanner.nextInt());
+
+        System.out.println("Enter the coordinates of C ");
+        pointC.setX(scanner.nextInt());
+        pointC.setY(scanner.nextInt());
+
+        System.out.println("Enter the coordinates of D ");
+        pointD.setX(scanner.nextInt());
+        pointD.setY(scanner.nextInt());
+
+        Quadrilateral quadrilateral= new Quadrilateral(pointA, pointB, pointC,pointD);
+        QuadrilateralCalculations quadrilateralCalculations = new QuadrilateralCalculations();
+        double perimeter=quadrilateralCalculations.perimeter(quadrilateral);
+        System.out.println("Perimeter is " + perimeter);
+
+        System.out.println("Enter the value of sinus ");
+        sinus= scanner.nextInt();
+        double area=quadrilateralCalculations.area(quadrilateral, sinus);
+        System.out.println("Area is " + area);
+
+    }
+}
+
+
+
---
+++ src/QuadrilateralCalculations.java
@@ -0,0 +1,29 @@
+import static java.lang.Math.*;
+
+public class QuadrilateralCalculations {
+
+    public double perimeter(Quadrilateral quadrilateral){
+    Point a=quadrilateral.getA();
+    Point b=quadrilateral.getB();
+    Point c=quadrilateral.getC();
+    Point d=quadrilateral.getD();
+    double ab=sqrt(pow((b.getX()-a.getX()),2) + pow((b.getY()-a.getY()),2));
+    double bc=sqrt(pow((c.getX()-b.getX()),2) + pow((c.getY()-b.getY()),2));
+    double cd=sqrt(pow((d.getX()-c.getX()),2) + pow((d.getY()-c.getY()),2));
+    double da=sqrt(pow((a.getX()-d.getX()),2) + pow((a.getY()-d.getY()),2));
+
+      return ab+bc+cd+da;
+
+    }
+    public double area(Quadrilateral quadrilateral, double sinus){
+        Point a=quadrilateral.getA();
+        Point b=quadrilateral.getB();
+        Point c=quadrilateral.getC();
+        Point d=quadrilateral.getD();
+        double ac=sqrt(pow((c.getX()-a.getX()),2) + pow((c.getY()-a.getY()),2));
+        double bd=sqrt(pow((d.getX()-b.getX()),2) + pow((d.getY()-b.getY()),2));
+
+        return ac*bd*0.5*sin(sinus);
+    }
+}
+
