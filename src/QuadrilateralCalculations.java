328415a19a35c75630748b753b132c3962194b74
TiodorBurilkov <109085379+TiodorBurilkov@users.noreply.github.com>
2022-07-15 21:04:12 +0300

    Update
    Changed method name

--- src/QuadrilateralCalculations.java
+++ src/QuadrilateralCalculations.java
@@ -5,18 +5,18 @@ public class QuadrilateralCalculations {
 
     public double perimeter(Quadrilateral quadrilateral) {
         QuadrilateralDistance quadrilateralDistance = new QuadrilateralDistance();
-        double ab = quadrilateralDistance.pointCalculation(quadrilateral.getA(), quadrilateral.getB());
-        double bc = quadrilateralDistance.pointCalculation(quadrilateral.getA(), quadrilateral.getB());
-        double cd = quadrilateralDistance.pointCalculation(quadrilateral.getA(), quadrilateral.getB());
-        double da = quadrilateralDistance.pointCalculation(quadrilateral.getA(), quadrilateral.getB());
+        double ab = quadrilateralDistance.calculateDistance(quadrilateral.getA(), quadrilateral.getB());
+        double bc = quadrilateralDistance.calculateDistance(quadrilateral.getA(), quadrilateral.getB());
+        double cd = quadrilateralDistance.calculateDistance(quadrilateral.getA(), quadrilateral.getB());
+        double da = quadrilateralDistance.calculateDistance(quadrilateral.getA(), quadrilateral.getB());
         return ab + bc + cd + da;
 
     }
 
     public double area(Quadrilateral quadrilateral, double sinus) {
         QuadrilateralDistance quadrilateralDistance = new QuadrilateralDistance();
-        double ac = quadrilateralDistance.pointCalculation(quadrilateral.getA(), quadrilateral.getB());
-        double bd = quadrilateralDistance.pointCalculation(quadrilateral.getA(), quadrilateral.getB());
+        double ac = quadrilateralDistance.calculateDistance(quadrilateral.getA(), quadrilateral.getB());
+        double bd = quadrilateralDistance.calculateDistance(quadrilateral.getA(), quadrilateral.getB());
         return ac * bd * 0.5 * sin(sinus);
     }
 
--- src/QuadrilateralDistance.java
+++ src/QuadrilateralDistance.java
@@ -3,7 +3,7 @@ import static java.lang.Math.sqrt;
 
 public class QuadrilateralDistance {
 
-    public double pointCalculation(Point a, Point b) {
+    public double calculateDistance(Point a, Point b) {
         double ab = sqrt(pow((b.getX() - a.getX()), 2) + pow((b.getY() - a.getY()), 2));
         return ab;
     }
