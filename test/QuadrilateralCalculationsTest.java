9abfe709e3b3c2cf9422933ea76e04fa99dec2b9
TiodorBurilkov <109085379+TiodorBurilkov@users.noreply.github.com>
2022-07-20 14:56:52 +0300

    Updated test
    More complex figure

--- test/QuadrilateralCalculationsTest.java
+++ test/QuadrilateralCalculationsTest.java
@@ -11,19 +11,19 @@ class QuadrilateralCalculationsTest {
     void setUp() {
 
         sinus = 90;
-        this.test = new Quadrilateral(new Point(1, 1), new Point(2, 1), new Point(2, 2), new Point(1, 2));
+        this.test = new Quadrilateral(new Point(1, 1), new Point(5, 1), new Point(5, 6), new Point(3, 6));
     }
 
     @Test
     void perimeter() {
         QuadrilateralCalculations quadrilateralCalculations = new QuadrilateralCalculations();
-        assertEquals(4, quadrilateralCalculations.perimeter(test));
+        assertEquals(16, quadrilateralCalculations.perimeter(test));
     }
 
     @Test
     void area() {
         QuadrilateralCalculations quadrilateralCalculations = new QuadrilateralCalculations();
         double area = Math.round(quadrilateralCalculations.area(test, sinus) * 100) / 100;
-        assertEquals(1, area);
+        assertEquals(15  , area);
     }
 }
