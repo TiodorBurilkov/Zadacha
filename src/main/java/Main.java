3d9a9f5dc20e32700fde71370aa9d66fb2d61983
TiodorBurilkov <109085379+TiodorBurilkov@users.noreply.github.com>
2022-07-25 20:26:00 +0300

    Update
    Fixed previous problem and added error logging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,15 @@
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        BasicConfigurator.configure();
-
         Logger logger = LogManager.getLogger(Main.class);
+
 
         Scanner scanner = new Scanner(System.in);
         Point pointA = new Point();
@@ -39,6 +39,7 @@ public class Main {
         double perimeter = quadrilateralCalculations.perimeter(quadrilateral);
 
         logger.info("Perimeter is  " + perimeter);
+        logger.error("Error");
 
 
         logger.info("Enter the value of sinus ");
